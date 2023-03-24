12adbab4dd7e6f1915bdca3eb492b77589adce41
William M <artofmediel@gmail.com>
2023-03-24 11:30:34 +0800

    Add files via upload

---
+++ 006UserInput.py
@@ -0,0 +1,15 @@
+#str input
+name = input("What is your name? : ")
+
+print("Hello " + name)
+
+#int input
+age = int(input("How old are you? : "))
+age = age + 1
+
+print("You are " + str(age) + " years old")
+
+#float input
+height = float(input("How tall are you? : "))
+
+print("You are " + str(height) + "cm tall")
