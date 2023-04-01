50cc021e70de52ddc666bba209e1004b62ef4889
William M <artofmediel@gmail.com>
2023-04-01 13:06:25 +0800

    Add files via upload

---
+++ 021Functions.py
@@ -0,0 +1,11 @@
+# function - a block of code which is executed only when it is called
+
+def hello(first_name,last_name,age):
+    print("hello " + first_name + " " + last_name)
+    print("you are " + str(age) + " years old")
+    print("have a nice day!")
+
+name1 = "wiru"
+name2 = "mediel"
+
+hello(name1,name2,28)
