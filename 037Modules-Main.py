ee21936128e998705f64251a8a190bac0a29d9fd
William M <artofmediel@gmail.com>
2023-04-06 06:41:20 +0800

    Add files via upload

---
+++ 037Modules-Main.py
@@ -0,0 +1,19 @@
+# module - a file containing python code. May contain function, classes, etc.
+# used with modular programming, which is to separate a program into parts
+
+# ---------------------------------
+#import messages as msg
+
+#msg.hello()
+#msg.bye()
+
+# ---------------------------------
+# can also be like this
+from messages import hello,bye
+
+hello()
+bye()
+
+# --------------------------------
+#bonus
+help("modules")
---
+++ messages.py
@@ -0,0 +1,5 @@
+def hello():
+    print("Hello wiru")
+
+def bye():
+    print("Bye!")
