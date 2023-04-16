1310935792ed053799be4dd5f44261d10c20c772
William M <artofmediel@gmail.com>
2023-04-16 09:50:51 +0800

    Add files via upload

---
+++ 087KeyboardEvents.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def doSomething(event):
+    #print("you pressed: "+ event.keysym)
+    label.config(text=event.keysym)
+
+window = Tk()
+
+#window.bind(event.function)
+window.bind("<Key>",doSomething)
+
+label = Label(window,font=("Helvetica",100))
+label.pack()
+
+window.mainloop()
