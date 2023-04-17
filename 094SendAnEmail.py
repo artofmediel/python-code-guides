3e927024ceb839b6cd7ff3c82e3f0f141f5b3531
William M <artofmediel@gmail.com>
2023-04-18 07:53:59 +0800

    Add files via upload

---
+++ 094SendAnEmail.py
@@ -0,0 +1,30 @@
+import smtplib
+
+# use test gmail account here
+sender =    "sender@gmail.com"
+receiver =  "receiver@gmail.com"
+password =  "password123"
+subject =   "Python email test"
+body =  "I wrote an email."
+
+# header
+message = f"""From : Snoop Dogg{sender}
+To: Nicholas Cage{receiver}
+Subject: {subject}\n
+{body}
+"""
+
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+
+try:
+    server.login(sender,password)
+    print("Logged in...")
+    server.sendmail(sender,receiver, message)
+    print("Email has been sent!")
+
+except smtplib.SMTPAuthenticationError:
+    print("unable to sign in")
+
+#except smtplib.SMTPServerDisconnected:
+    #print("connection closed unexpectedly")
