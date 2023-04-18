990dfce754f61e6fb177db02a72cb77af0bd6565
William M <artofmediel@gmail.com>
2023-04-18 10:59:40 +0800

    Add files via upload

---
+++ 095RunWithCommandPrompt.py
@@ -0,0 +1,14 @@
+# -----------------------------------
+# run .py file with cmd
+# -----------------------------------
+# save file as .py (python file)
+# go to command prompt
+# navigate to directory w/ your file: cd c:[directory of python file]
+# NOTE: the python file directory needs to be in C drive in order to work
+# invoke python interpreter - script: python 095RunWithCommandPrompt.py
+
+print("Hello World!")
+
+name = input("What is your name?: ")
+
+print("Hello " + name)
