ec974453e3e4eb757f4d564e8565d9b4ad0beadd
William M <artofmediel@gmail.com>
2023-04-19 10:39:20 +0800

    Add files via upload

---
+++ 097PyFiletoExeFile.py
@@ -0,0 +1,11 @@
+# windows defender may prevent you from running
+# make sure pip and pyinstaller are installed / upgraded
+
+# cd to directory that contain you .py file
+# pyinstaller ...
+#       -F              (all in 1 file)
+#       -w              (removes terminal window)
+#       -i icon.ico     (adds custom icon to .exe)
+#       clock.py        (name of your main python file)
+
+# .exe is located in the dist folder
