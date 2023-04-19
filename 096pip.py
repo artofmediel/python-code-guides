fb4b630585949b5899b9386d727ccf3f2eee951d
William M <artofmediel@gmail.com>
2023-04-19 10:11:57 +0800

    Add files via upload

---
+++ 096pip.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------
+#   Python pip
+# -----------------------------------------------------
+#   pip = package manager for packages and modules from Python Packages Index
+#
+#   included for Python versions 3.4+
+#   open command prompt
+
+#   help:                       pip
+#   check:                      pip --version
+#   update:                     pip install --upgrade pip
+#   installed packages:         pip list
+#   check outdated packages:    pip list --outdated
+#   install a package:          pip install "package name"
+# -----------------------------------------------------
+
